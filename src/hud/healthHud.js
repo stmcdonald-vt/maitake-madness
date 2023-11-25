8efab8c67d576c2b95fc0f2c350bf8d4650d0450
Sean McDonald <stmcdonald@vt.edu>
2023-11-24 22:30:08 -0500

    health bar

--- src/hud/healthHud.js
+++ src/hud/healthHud.js
@@ -1,17 +1,18 @@
 import entityManager from "../managers/entityManager";
 import gp5 from "../sketch";
-
+import HealthBar from "./healthBar";
 export default class HealthHud {
     constructor(x, y) {
         this.x = x;
         this.y = y;
+        this.healthBar = new HealthBar(this.x + 85, this.y - 17, entityManager.gnome);
     }
 
     display() {
-        const gnome = entityManager.gnome;
         gp5.push();
         gp5.fill('black');
-        gp5.text(`Health: ${gp5.floor(gnome.health / gnome.startHealth * 100)}%`, this.x, this.y);
+        gp5.text(`Health:`, this.x, this.y);
         gp5.pop();
+        this.healthBar.display();
     }
 }
